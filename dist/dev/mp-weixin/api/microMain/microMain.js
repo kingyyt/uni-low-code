0ffee338c0dba3a3d12efcdc0c19a69d9d4e55f1
kingyyt <yeyiteng@163.com>
2024-06-21 15:30:18 +0800

    更新模版

--- dist/dev/mp-weixin/api/microMain/microMain.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const http_httpRequest = require("../../http/httpRequest.js");
-const GetJsonListDetail = (id) => http_httpRequest.Http.getRequest(`${"/api/uni/jsonListDetail/"}${id}/`);
-exports.GetJsonListDetail = GetJsonListDetail;
