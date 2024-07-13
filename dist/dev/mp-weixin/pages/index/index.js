a434d92a3a5b4cb3d37428800a0d14e5fae88284
kingyyt <yeyiteng@163.com>
2024-07-13 14:59:52 +0800

    tabbar

--- dist/dev/mp-weixin/pages/index/index.js
+++ dist/dev/mp-weixin/pages/index/index.js
@@ -147,12 +147,20 @@ createPage(_pages_index_index_vue__WEBPACK_IMPORTED_MODULE_2__["default"]);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/asyncToGenerator */ "./node_modules/.pnpm/@babel+runtime@7.24.7/node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js");
-/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/.pnpm/@dcloudio+vue-cli-plugin-uni@2.0.2-4010520240507001_copy-webpack-plugin@9.1.0_webpack@4.47.0__postcss@8.4.38/node_modules/@dcloudio/vue-cli-plugin-uni/packages/@babel/runtime/regenerator/index.js");
-/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _api_index__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/api/index */ "./src/api/index.js");
+/* harmony import */ var _babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/defineProperty */ "./node_modules/.pnpm/@babel+runtime@7.24.7/node_modules/@babel/runtime/helpers/esm/defineProperty.js");
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/asyncToGenerator */ "./node_modules/.pnpm/@babel+runtime@7.24.7/node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/.pnpm/@dcloudio+vue-cli-plugin-uni@2.0.2-4010520240507001_copy-webpack-plugin@9.1.0_webpack@4.47.0__postcss@8.4.38/node_modules/@dcloudio/vue-cli-plugin-uni/packages/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _api_index__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @/api/index */ "./src/api/index.js");
 
 
+
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -165,36 +173,45 @@ __webpack_require__.r(__webpack_exports__);
 //
 
 
-/* harmony default export */ __webpack_exports__["default"] = ({
+/* harmony default export */ __webpack_exports__["default"] = ((0,_babel_runtime_helpers_defineProperty__WEBPACK_IMPORTED_MODULE_0__["default"])({
   data: function data() {
     return {
-      title: "Hello"
+      title: "Hello",
+      active: 0
     };
+  },
+  methods: {
+    aaa: function aaa(event) {
+      console.log(123123);
+      // event.detail 的值为当前选中项的索引
+      this.setData({
+        active: event.detail
+      });
+    }
   },
   onLoad: function onLoad() {
     this.init();
-  },
-  methods: {
-    init: function init() {
-      return (0,_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__["default"])( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default().mark(function _callee() {
-        var res;
-        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default().wrap(function _callee$(_context) {
-          while (1) switch (_context.prev = _context.next) {
-            case 0:
-              _context.next = 2;
-              return (0,_api_index__WEBPACK_IMPORTED_MODULE_2__.GetJsonListDetail)(1);
-            case 2:
-              res = _context.sent;
-              console.log(res);
-            case 4:
-            case "end":
-              return _context.stop();
-          }
-        }, _callee);
-      }))();
-    }
   }
-});
+}, "methods", {
+  init: function init() {
+    return (0,_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__["default"])( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default().mark(function _callee() {
+      var res;
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default().wrap(function _callee$(_context) {
+        while (1) switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return (0,_api_index__WEBPACK_IMPORTED_MODULE_3__.GetJsonListDetail)(1);
+          case 2:
+            res = _context.sent;
+            console.log(res);
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }, _callee);
+    }))();
+  }
+}));
 
 /***/ }),
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,9 +2,15 @@
   <view class="content">
     <image class="logo" src="/static/logo.png"></image>
     <view>
-      <text class="title">{{ title }}</text>
+      <text @click="aaa" class="title">{{ title }}</text>
       <van-button>123</van-button>
     </view>
+    <van-tabbar :active="active" @change="aaa">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </view>
 </template>
 
@@ -14,6 +20,7 @@ export default {
   data() {
     return {
       title: "Hello",
+      active: 0,
     };
   },
   onLoad() {
@@ -23,6 +30,11 @@ export default {
     async init() {
       const res = await GetJsonListDetail(1);
       console.log(res);
+    },
+    aaa(event) {
+      console.log(123123);
+      // event.detail 的值为当前选中项的索引
+      this.active = event.detail;
     },
   },
 };
